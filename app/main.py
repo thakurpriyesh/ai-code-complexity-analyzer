01d091e30553007af638e234e13317b05e5f5bf4
thakurpriyesh <thakurpriyesh01@gmail.com>
2026-05-24 19:12:46 +0530

    Fixed path issues

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if root_path not in sys.path:
+    sys.path.append(root_path)
+
 import streamlit as st
 import pandas as pd
 from streamlit_ace import st_ace
